9ef620ee046676f2de81e5d60bbd1fc748eb4b18
starrio <tarriosantiago@gmail.com>
2018-03-14 18:33:56 +0100

    jupyter notebook

--- algorithm.py
+++ algorithm.py
@@ -24,8 +24,8 @@ class MOEC:
         self.z_star = np.array([min([ind[o] for ind in self.population]) for o in range(self.problem.n_obj)])
 
 
-    def get_neigbours(self,v1,sps,lambdas,T):
-        return sorted(lambdas, key = lambda v: np.linalg.norm(v1-lambdas[v]))[:T]
+    def get_neighbours(self,v1,sps,lambdas,T):
+        return sorted(sps, key = lambda v: np.linalg.norm(v1-lambdas[v]))[:T]
 
     def run(self):
         for g in range(self.generations):
@@ -33,12 +33,13 @@ class MOEC:
                 y = self.recombination(i)
                 y_performance = self.problem.func(y)
                 self.population[i] = y
-                for j in self.neigbours[i]:
+                for j in self.neighbours[i]:
                     if(self.tchebycheff(y,self.lambda_vectors[j]) <= self.tchebycheff(self.population[j],self.lambda_vectors[j])):
                         self.population[j]=y
+        return self.population
     
     def recombination(self,i):
-        return 0
+        return self.population[i]
     
     def tchebycheff(self,x,lamb):
         return 0
